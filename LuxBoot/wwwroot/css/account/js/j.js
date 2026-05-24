a17358ed9c566ef4b76903fb5149276e0e4d8b90
Adenis <64251538+Ad3n1s@users.noreply.github.com>
2026-05-24 19:29:14 +0200

    Add files via upload

---
+++ LuxBoot/wwwroot/css/account/js/j.js
@@ -0,0 +1,39 @@
+﻿const ctxx = document.getElementById('attackChart');
+
+
+const getdata = document.getElementById('getchartdata');
+
+const values = getdata.dataset.value
+    .split(',')
+    .map(Number);
+
+new Chart(ctxx, {
+    type: 'line',
+    data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+            label: 'Attacks',
+            data: values,
+            borderColor: '#00e5ff',
+            backgroundColor: 'rgba(0,229,255,0.1)',
+            tension: 0.4
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'white'
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: { color: 'white' }
+            },
+            y: {
+                ticks: { color: 'white' }
+            }
+        }
+    }
+});
